227a609ee3e33168fce6f47da0f767a515ad214e
Márcio Sodré <marciohector.06@gmail.com>
2026-02-27 17:06:54 -0300

    feat: evolucao do codigo base e estrutura inicial

---
+++ tela-compras/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [novoItem, setNovoItem] = useState('')
+  const [listaCompras, setListaCompras] = useState([])
+
+  const adicionarItem = () => {
+    // R&N (HdU 01): O sistema não deve permitir o cadastro de itens sem nome
+    if (novoItem.trim() === '') {
+      alert('O nome do item é obrigatório!')
+      return
+    }
+
+    setListaCompras([...listaCompras, novoItem])
+    setNovoItem('')
+  }
+
+  return (
+    <div>
+      <h1>🛒 Controle de Necessidades e Compras</h1>
+      
+      <div>
+        <input 
+          type="text" 
+          placeholder="Digite o nome do item..." 
+          data-testid="input-novo-item"
+          value={novoItem} 
+          onChange={(evento) => setNovoItem(evento.target.value)} 
+        />
+        <button data-testid="btn-salvar-item" onClick={adicionarItem}>
+          Salvar
+        </button>
+      </div>
+
+      <ul>
+        {listaCompras.map((item, index) => (
+          <li key={index} data-testid="item-lista">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
